fa643871a3ecafe7377f165cee8e58b1a33f7a97
Meg Roth <megroth2@gmail.com>
2023-09-29 21:21:14 -0600

    add built in methods answers

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,29 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The include? method is called on the string object "Hello World"
+# The "Hello" argument is passed to see if the string object includes Hello
+# The return value is true
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string "Hello World"
+# The "Hello" argument is passed to see if the string ends with "rld"
+# The return value is false
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string "Hello World"
+# The "rld" argument is passed to see if the string ends with "rld"
+# The return value is true
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the integer 12
+# No arguments are passed; it simply checks if the integer is even and outputs a boolean value
+# The return value is true
 12.even?
 
-
+# The next method is called on the integer 18
+# No arguments are passed; it simply outputs the next integer
+# The return value is 19
 18.next
 
 
@@ -39,6 +49,19 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+# The << method is called on the candle_scent variable, which stores the string object "vanilla".
+# The << method returns a concatenation of the candle_scent string object and the argument passed in.
+# In this example, the return value is "vanilla scented candle".
+# The puts command prints the return value of the << method to the console.
+candle_scent = "vanilla"
+puts candle_scent << " scented candle"
+
+# The prepend method is called on the last_name variable, which stores the string object "Roth".
+# The prepend method returns a concatenation of the two arguments passed in and the last_name string object.
+# In this example, the return value is "Miss Meg Roth".
+# The puts command prints the return value of the prepend method to the console.
+last_name = "Roth"
+puts last_name.prepend("Miss ","Meg ")
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
@@ -47,6 +70,19 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# The product method is called on the rent variable, which stores the integer 1000.
+# The product method returns the value of rent times 12.
+# In this example, the return value is 12000.
+# The puts command prints the return value of the product method to the console.
+rent = 1000
+puts rent*12
+
+# The quotient method is called on the annual_salary variable, which stores the integer 50000.
+# The quotient method returns the value of annual_salary divided by 12.
+# In this example, the return value is 4166.
+# The puts command prints the return value of the quotient method to the console.
+annual_salary = 50000
+puts annual_salary/12
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +90,17 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The empty? method is called on the kinds_of_pet variable, which stores the array of strings cat, dog, fish, and hamster.
+# The empty? method returns true if the kinds_of_pet array is empty.
+# In this example, the return value is false because the kinds_of_pet variable is not empty.
+# The puts command prints the return value of the empty? method to the console.
+kinds_of_pet = ["cat", "dog", "fish", "hamster"]
+puts kinds_of_pet.empty?
+
+# The count method is called on the groceries variable, which stores the array of strings butter, milk, chicken, and garlic.
+# The count method returns the number of elements included in the groceries array.
+# In this example, the return value is 4 because there are 4 elements in the array.
+# The puts command prints the return value of the count method to the console.
+groceries = ["butter", "milk", "chicken", "garlic"]
+puts groceries.count
